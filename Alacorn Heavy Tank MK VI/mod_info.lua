41f441c565dbcdc0a861acd79c7e10f3a4a99c10
AzrielSukhanov <jasonedwinoliver@gmail.com>
2015-04-29 20:57:00 -0500

    Revert "Revert "Initial commit to Github""
    This reverts commit 33958ef43faa70b888ee4d01328096069c5ba4cf.

---
+++ Alacorn Heavy Tank MK VI/mod_info.lua
@@ -0,0 +1,12 @@
+name = "Alacorn Heavy Tank MK VI"
+version = 1
+copyright = "Copyright � 2006, Gas Powered Games"
+description = "UEF T2 Alacorn Heavy Tank MK VI"
+author = "Gas Powered Games"
+url = "http://www.gaspoweredgames.com"
+uid = "74B9EAB2-E851-11DB-Z1F1-F2C855D09593"
+
+exclusive = false
+
+ui_only = false
+
